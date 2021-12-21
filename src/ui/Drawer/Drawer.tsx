4cc3d472b8f1782742aa8dbcb738a3a09fce8f50
Georgiy Filippov <g.filippov777@gmail.com>
2021-12-21 08:32:30 +0500

    feat: add NavLinks

--- src/ui/Drawer/Drawer.tsx
+++ src/ui/Drawer/Drawer.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import cls from './Drawer.module.css'
 import Backdrop from '../Backdrop/Backdrop'
+import { NavLink } from 'react-router-dom'
 
-// TODO: replace links
-const links = ['home', 'link-1', 'link-2']
+const links = [
+  { to: '/', label: 'Quiz List' },
+  { to: '/auth', label: 'Auth' },
+  { to: '/quiz-creator', label: 'Quiz Creator' },
+]
 
 type Props = {
   isMenuOpen: boolean
@@ -15,6 +19,11 @@ const Drawer = (props: Props) => {
   const classes = [cls.wrapper]
   if (!isMenuOpen) classes.push(cls.close)
 
+  const activeStyle = { color: '#7969e6' }
+  const notActiveStyle = { color: '#363d54' }
+
+  const handleClickCloseMenu = (): void => onClickBackdrop()
+
   return (
     <>
       <nav className={classes.join(' ')}>
@@ -22,9 +31,16 @@ const Drawer = (props: Props) => {
           {links.map((link, idx) => {
             return (
               <li key={idx} className={cls.listItem}>
-                <a href={`/${link}`} className={cls.link}>
-                  {link.toUpperCase()}
-                </a>
+                <NavLink
+                  to={link.to}
+                  className={cls.link}
+                  style={({ isActive }) =>
+                    isActive ? activeStyle : notActiveStyle
+                  }
+                  onClick={handleClickCloseMenu}
+                >
+                  {link.label}
+                </NavLink>
               </li>
             )
           })}
