49791485dfda22226cd78f03496c87936044ef71
Georgiy Filippov <g.filippov777@gmail.com>
2021-12-23 11:33:26 +0500

    feat: add login form

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import cls from './Auth.module.css'
+import Button from '../../ui/Button/Button'
 
 const Auth = () => {
+  const handleLogin = (): void => {}
+
+  const handleRegister = (): void => {}
+
+  const handleSubmit = (evt: React.FormEvent<HTMLFormElement>): void => {
+    evt.preventDefault()
+  }
+
   return (
-    <div>
-      <h2>Auth</h2>
+    <div className={cls.wrapper}>
+      <h2 className={cls.heading}>Login Form</h2>
+
+      <form className={cls.form} onSubmit={handleSubmit}>
+        <input type='text' />
+        <input type='text' />
+
+        <div className={cls.controlsWrapper}>
+          <Button onClickButton={handleLogin} variant={'success'}>
+            Login
+          </Button>
+          <Button
+            onClickButton={handleRegister}
+            variant={'primary'}
+            cssStyles={cls.controlRegister}
+          >
+            Register
+          </Button>
+        </div>
+      </form>
     </div>
   )
 }
