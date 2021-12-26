0b6b13af1afd46b479faf3b3cca9df84e1fcca62
Georgiy Filippov <g.filippov777@gmail.com>
2021-12-26 16:21:22 +0500

    feat: add QuizCreator base form

--- src/components/QuizCreator/QuizCreator.tsx
+++ src/components/QuizCreator/QuizCreator.tsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import cls from './QuizCreator.module.css'
+import Button from '../../ui/Button/Button'
 
 const QuizCreator = () => {
+  const handleAddField = (): void => {}
+  const handleCreateQuiz = (): void => {}
+
   return (
-    <div>
-      <h2>Quiz Creator</h2>
+    <div className={cls.wrapper}>
+      <h2 className={cls.heading}>Quiz Creator</h2>
+
+      <form className={cls.form}>
+        <input type='text' />
+        <br />
+
+        <input type='text' />
+        <input type='text' />
+        <input type='text' />
+        <input type='text' />
+        <select>{/*  */}</select>
+
+        <div className={cls.controlsWrapper}>
+          <Button onClickButton={handleAddField} variant={'primary'}>
+            Add Field
+          </Button>
+          <Button
+            onClickButton={handleCreateQuiz}
+            variant={'success'}
+            cssStyles={cls.mr0}
+          >
+            Create Quiz
+          </Button>
+        </div>
+      </form>
     </div>
   )
 }
