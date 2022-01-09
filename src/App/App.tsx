d58bfeb9dbc39c31a595e16353314ddf26658c3f
Georgiy Filippov <g.filippov777@gmail.com>
2022-01-09 23:41:40 +0500

    feat: add AuthCont & auth requests

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -4,7 +4,7 @@ import QuizCont from '../containers/QuizCont/QuizCont'
 import NavBar from '../ui/NavBar/NavBar'
 import { Route, Routes } from 'react-router-dom'
 import WelcomeScreen from '../components/WelcomeScreen/WelcomeScreen'
-import Auth from '../components/Auth/Auth'
+import AuthCont from '../containers/AuthCont/AuthCont'
 import QuizCreatorCont from '../containers/QuizCreatorCont/QuizCreatorCont'
 import { LogoLink } from '../ui/LogoLink/LogoLink'
 import { AppRoute } from '../constants'
@@ -19,7 +19,7 @@ const App = () => (
     <main>
       <Routes>
         <Route path={AppRoute.HOME} element={<WelcomeScreen />} />
-        <Route path={AppRoute.LOGIN} element={<Auth />} />
+        <Route path={AppRoute.LOGIN} element={<AuthCont />} />
         <Route path={AppRoute.QUIZZES} element={<QuizCont />} />
         <Route path={AppRoute.QUIZ_CREATOR} element={<QuizCreatorCont />} />
       </Routes>
