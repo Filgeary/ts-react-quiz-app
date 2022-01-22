f6ac4cbe7e8f6cda72a120fbc0234cb631a2324c
Georgiy Filippov <g.filippov777@gmail.com>
2022-01-22 05:15:26 +0500

    feat: add typed redux hooks

--- src/store/store.ts
+++ src/store/store.ts
@@ -11,3 +11,7 @@ export const store = configureStore({
 })
 
 setupListeners(store.dispatch)
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
